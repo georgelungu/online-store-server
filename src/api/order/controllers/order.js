fe223e37e6b9fc34f1ff20118f901e7502444be9
George Lungu <george.lungu1896@gmail.com>
2023-12-03 23:57:55 +0200

    managed to import stripe in server

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,12 +1,13 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
 'use strict';
 
 // left at 02:54:56
-// possible error when importing stripe like that
-// stripe is installed only in server.
 
 // const token = import.meta.env.VITE_APP_API_TOKEN;
-const stripe = require('stripe')(process.env.STRIPE_KEY);
-
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 /**
  * order controller
