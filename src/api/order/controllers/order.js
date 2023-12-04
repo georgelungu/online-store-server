4cb4ef5cde605ee415a92214ebea4c6b94207896
George Lungu <george.lungu1896@gmail.com>
2023-12-04 23:23:01 +0200

    further work that i don't understand with stripe

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,10 +1,10 @@
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.STRIPE_KEY);
-
 'use strict';
 
-// left at 02:54:56
+const Stripe = require('stripe');
+
+const stripe = new Stripe.default(process.env.STRIPE_KEY);
+
+// left at 02:57:58
 
 // const token = import.meta.env.VITE_APP_API_TOKEN;
 // const stripe = require('stripe')(process.env.STRIPE_KEY);
@@ -15,4 +15,21 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+    async create(context) {
+        const { email, products } = context.request.body
+        try 
+        {
+            const session = stripe.checkout.create({
+                mode: 'payment',
+                success_url: `${process.env.CLIENT_URL}?success=true`,
+                cancel_url: `${process.env.CLIENT_URL}/?success=false`,
+            })
+        } 
+        catch (error) 
+        {
+            context.response.status = 500
+            return error
+        }
+    }
+}));
