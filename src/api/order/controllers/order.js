9b56951750efab0f536e514999c4d6db7a348b33
George Lungu <george.lungu1896@gmail.com>
2023-12-08 19:56:53 +0200

    finished the backend tutorial part

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -4,10 +4,8 @@ const Stripe = require('stripe');
 
 const stripe = new Stripe.default(process.env.STRIPE_KEY);
 
-// left at 02:57:58
-
-// const token = import.meta.env.VITE_APP_API_TOKEN;
-// const stripe = require('stripe')(process.env.STRIPE_KEY);
+// left at 03:00:00
+// left at 03:09:30
 
 /**
  * order controller
@@ -18,13 +16,47 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::order.order', ({ strapi }) => ({
     async create(context) {
         const { email, products } = context.request.body
+
+        const lineItems = await Promise.all(
+            products.map(async (product) => {
+                const item = await strapi
+                .service("api::product.product")
+                .findOne(product.id)
+
+                return{
+                    price_data:{
+                        currency: "usd",
+                        product_data: {
+                            name: product.title,
+                        },
+                        unit_amount: product.price * 100
+                    },
+                    quantity: product.quantity
+                }
+            })
+        )
+
         try 
         {
-            const session = stripe.checkout.create({
+            const session = await stripe.checkout.sessions.create({
                 mode: 'payment',
                 success_url: `${process.env.CLIENT_URL}?success=true`,
                 cancel_url: `${process.env.CLIENT_URL}/?success=false`,
+                line_items: lineItems,
+                shipping_address_collection: {allowed_countries:["US", "CA"]},
+                payment_method_types: ["card"], // don't know how to also add cash
             })
+
+            await strapi.service("api::order.order").create({
+                data:
+                {
+                    products, 
+                    stripeId: session.id
+                    // normally was stripeId: session.id
+                }
+            })
+
+            return { stripeSession: session }
         } 
         catch (error) 
         {
